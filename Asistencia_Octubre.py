189867494f94dcd136d89445717d58dba2224f1d
Carolina Belén Delgado <98441984+programacioncarolina1980@users.noreply.github.com>
2022-10-20 11:21:04 -0300

    Update Asistencia_Octubre.py
    Actividad 05 phyton

--- Asistencia_Octubre.py
+++ Asistencia_Octubre.py
@@ -21,3 +21,16 @@ base = int(input('Digite el número para la base del rectángulo: '))
 altura = int(input('Digite el número para la altura del rectángulo: '))
 rectangulo1 = Rectangulo(base, altura)
 print(f'El área del rectángulo es: {rectangulo1.calcular_area()}')
+
+
+# Carolina Belen Delgado
+# Ejercicio 05: Crear una funcion para sumar valores recibidos de tipo numericos,
+# Utilizando argumentos variabes args como parametro de a
+# Funcion y agregar como resultado a suma de todos os valores pasados
+# como argumentos.
+# Definimos una funcion.
+def sumar_valor(args, in_args=):# Recibimos una cantidad de parametros indefinidos
+ result=0
+ # Iteramos cada elemento
+ for valor in_args:
+ result
